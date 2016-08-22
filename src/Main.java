d8b00fe6b0518593bb83ad288e52cc284fb1c8ec
John Jenkins <jjenksy@gmail.com>
2016-08-22 15:02:38 -0500

    created a method to log the passed arguments to the console.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+/**
+ * John Jenkins COSC
+ *
+ */
 public class Main {
 
+    /**
+     * main method
+     * @param args
+     */
     public static void main(String[] args) {
+
+
         for(int i=1; i<11; i++){
             System.out.println("Count is: " + i);
         }
-        //add comments for testing
-        System.out.println("Hello World!");
-        System.out.println("Test branch added!");
+        add("Hello World!");
+        add("Test branch added!");
+        add("Hello World!");
+
+}
+
+    public static void add(String a)
+    {
+        System.out.println(a);
+
     }
 }
