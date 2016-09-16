8251f59bc58a0ce92fda45bcb86a2e703a0914c6
John Jenkins <jjenksy@gmail.com>
2016-09-16 11:49:48 -0500

    Started chapter 5 excercises

---
+++ src/classExercises/LoopAndSentinal.java
@@ -0,0 +1,18 @@
+package classExercises;
+
+/**
+ * Created by jjenkins on 9/16/2016.
+ */
+public class LoopAndSentinal {
+    public static void main (String[] args) {
+
+        for(int i =0; i<=200; i++){
+            if(i%2==0){
+                System.out.print(i+ " ");
+            }else if(i%3==0){
+                System.out.print(i+" ");
+
+            }
+        }
+    }
+}
