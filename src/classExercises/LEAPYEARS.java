da819faab28ad8c3a9a357e0466bb3b93a3589ea
John Jenkins <jjenksy@gmail.com>
2016-09-29 14:10:10 -0500

    added loop pattern and leapyears sample

---
+++ src/classExercises/LEAPYEARS.java
@@ -0,0 +1,37 @@
+package classExercises;
+
+import java.util.Scanner;
+
+/**
+ * Created by jjenkins on 9/29/2016.
+ *Write a program that displays all leap years, 10 per line, from two positive interger year periods entered by
+ * the user,
+ */
+public class LEAPYEARS {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int noOfLeapYear = 0;
+        System.out.print("Enter the starting year:");
+        int startYear = stdin.nextInt();
+        System.out.print("Enter the ending year:");
+        int endYear = stdin.nextInt();
+
+
+        //In general terms the algorithm for calculating a leap year is as follows... A year will be a leap year if it is divisible by 4 but not by 100.
+        // If a year is divisible by 4 and by 100, it is not a leap year unless it is also divisible by 400.
+        for (int year = startYear; year <= endYear; year++) {
+            boolean isLeapYear = (year % 4 == 0);
+
+            if (isLeapYear) {
+                noOfLeapYear++;
+                System.out.print(year + " ");
+                if (noOfLeapYear % 10 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+
+        System.out.println("\nThere were "+noOfLeapYear+" leap years from "+startYear+" to "+endYear);
+
+    }
+}
--- src/classExercises/LOOPPATTERN.java
+++ src/classExercises/LOOPPATTERN.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 /**
  * Created by jjenkins on 9/16/2016.
  */
-public class LoopAndSentinal {
-
-
+public class LOOPPATTERN {
 
     //pattern while loop problem
             public static void main(String[] args) {
-                System.out.print("Enter the number of lines:");
-                Scanner input = new Scanner(System.in);
+                System.out.print("Enter an integer to be a limit of the pattern:");
+                Scanner stdin = new Scanner(System.in);
 
                 // get the total number of lines n.
-                int n = input.nextInt();
+                int n = stdin.nextInt();
 
                 // Loop through the lines from 1 to n
-                System.out.println("Pattern A");
+                System.out.println("Pattern A:");
                 for (int i = 1; i <= n; i++) {
 
                     // Printing number increamentally from 1 to line number j
@@ -28,12 +26,11 @@ public class LoopAndSentinal {
                     System.out.println();
 
                 }
+                System.out.println();
 
                 // Loop through the lines from 1 to n (i=0 to n-1
-                System.out.println("Pattern B");
+                System.out.println("Pattern B:");
                 for (int i = 0; i < n; i++) {
-
-
 
                     // Printing number increamentally from 1 to line number j
                     for (int j = 1; j <= n - i; j++) {
@@ -42,22 +39,24 @@ public class LoopAndSentinal {
                     System.out.println();
 
                 }
+
+                System.out.println();
                 //Loop through the lines from 1 to n (i=0 to n-1)
-                System.out.println("Pattern C");
+                System.out.println("Pattern C:");
                 for (int i = 1; i <= n; i++) {
 
                     // Printing number increamentally from 1 to line number j
                     for (int j = 1; j <= i; j++) {
-                        System.out.print(i-j + " ");
+                        System.out.print(i - j+1 + " ");
                     }
                     System.out.println();
 
                 }
 
-
+                System.out.println();
 
                 //Loop through the lines from 1 to n
-                System.out.println("Pattern D");
+                System.out.println("Pattern D:");
                 for (int i = 0; i < n; i++) {
                     // Printing number increamentally from 1 to line number j
                     for (int j = 1; j <= n - i; j++) {
