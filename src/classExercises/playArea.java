dc197836613cfba46a88f10285fdb4126e58671b
John Jenkins <jjenksy@gmail.com>
2016-11-07 14:04:32 -0600

    added Collections and Generics

--- src/classExercises/playArea.java
+++ src/classExercises/playArea.java
@@ -6,14 +6,23 @@ package classExercises;
  * Created by jjenkins on 9/18/2016.
  */
 public class playArea {
-    public static void main (String[] args){
 
-        for (char outerChar='a'; outerChar<='e'; outerChar++){
-            for (char innerChar='a'; innerChar<='e'; innerChar++){
-              System.out.println(outerChar +"" + innerChar);
-            }
-        }
-
-
+    static int number2 = getValue();
+    static int number1 = 10;
+    static int getValue(){
+        return number1;
     }
+
+    static int doSum(){return number1+number2;}
+    static int doMinus(){return number1-number2;}
+    public static void main (String[] args) {
+
+        System.out.println(doSum());
+        System.out.println(doMinus());
+
+
+     }
+
+
+
 }
