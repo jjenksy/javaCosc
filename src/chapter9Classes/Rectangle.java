ec3c8c0f3003d34b7f46568841dd0ad7de2c8c8b
John Jenkins <jjenksy@gmail.com>
2016-11-18 10:58:16 -0600

    completed chapter 9

--- src/chapter9Classes/Rectangle.java
+++ src/chapter9Classes/Rectangle.java
@@ -1,15 +1,29 @@
 package chapter9Classes;
 
 /**
- * Created by jjenkins on 11/14/2016.
+ * * COSC 1173 Programming Lab
+ * @author Name: John Jenkins
+ * Data: 11/18/2016
+ * Refer to the example code of the Circle class in Chapter 8.2, design a class named Rectangle to
+ * represent a rectangle. The class contains:
+ * Two double data fields named width and length that specify the width and length of the
+ * rectangle. The default values are 0.0 for both width and length.
+ * A no-arg constructorthat creates a default rectangle.
+ * A constructor that creates a rectangle with the specified width and length.
+ * A method named getArea() that returnsthe area of this rectangle.
+ * A method named getPerimeter() that returnsthe perimeter.
+ * Accessor and Mutator methodsfor length and width;
+ * Make sure that your constructors and mutator methods can check the new length and width to make
+ * sure only positive values can be used in your code, otherwise print out error message to the user.
  */
 public class Rectangle {
+    //private varibles
     private double width;
     private double length;
 
     //default final values to avoid using magic numbers
-    public static final int DEFAULT_WIDTH =1;
-    public static final int DEFAULT_LENGTH =1;
+    public static final int DEFAULT_WIDTH =0;
+    public static final int DEFAULT_LENGTH =0;
     public static final int NON_NEGATIVE = 0;
     /**
      * Default no-arg constructor
@@ -20,9 +34,16 @@ public class Rectangle {
     }
     // constructor with parameter
     public Rectangle(double width, double length) {
-        //todo check to insure positive
-        this.width = width;
-        this.length = length;
+
+        //check to see if length and width ar not negative
+        if(width > 0 && length > 0){
+            this.width = width;
+            this.length = length;
+        }else{
+            System.out.println("The width and length cannot be negative the default is set to width "+DEFAULT_WIDTH+
+            " length " + DEFAULT_LENGTH);
+        }
+
     }
 
     //setters/accessors
@@ -55,14 +76,6 @@ public class Rectangle {
         return 2*(length +width);
     }
 
-    /**
-     * Print out the data to the console
-     */
-    public void RestangleToString(String name){
-        System.out.println("The area of "+name + " "  + width +" x " +length+" Rectangle is " +getArea());
-       System.out.println("The perimeter of a "+name + " " + width +" x " + length+" Rectangle is " + getPerimeter());
-
-    }
 
 
 
