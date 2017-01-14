4be6fbd4bd92d052b934b4134013282ae45baa0d
jenksy <jjenksy@gmail.com>
2017-01-14 13:40:10 -0600

    updated hashMap

--- src/Chapter10/MyInteger.java
+++ src/Chapter10/MyInteger.java
@@ -24,27 +24,6 @@ package Chapter10;
  */
 
 public class MyInteger {
-    //todo fix the static test method to use the class
-    public static void main(String[] args) {
-        MyInteger n1 = new MyInteger(5);
-        System.out.println("n1 is even? " + n1.isEven());
-        System.out.println("n1 is prime? " + n1.isPrime());
-        System.out.println("15 is prime? " + MyInteger.isPrime(15));
-
-        char[] chars = {'3', '5', '3', '9'};
-        System.out.println(MyInteger.parseInt(chars));
-
-        String s = "3539";
-        System.out.println(MyInteger.parseInt(s));
-
-        MyInteger n2 = new MyInteger(24);
-        System.out.println("n2 is odd? " + n2.isOdd());
-        System.out.println("45 is odd? " + MyInteger.isOdd(45));
-
-        System.out.println("n1 is equal to n2? " + n1.equals(n2));
-        System.out.println("n1 is equal to 5? " + n1.equals(5));
-
-    }
 
     //stores the value represented by the object
     int value;
