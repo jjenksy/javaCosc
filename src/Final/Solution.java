7719aa0507dba5dea53befcf7a3f815f98d46482
John Jenkins <jjenksy@gmail.com>
2017-01-17 14:39:35 -0600

    add new solution class to Final

--- src/Final/Solution.java
+++ src/Final/Solution.java
@@ -1,6 +1,5 @@
 package Final;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -8,49 +7,14 @@ import java.util.Scanner;
  */
 public class Solution {
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        ArrayList<String> list = new ArrayList<>();
-
-        while (n>0){
-            list.add(input.next());
-            n--;
-        }
-
-
-        for (int i = 0; i < list.size(); i++) {
-            shitPrinter(list.get(i));
-            if((!list.isEmpty())){
-                System.out.println();
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
             }
         }
-
-
-
-
-
-
     }
 
 
-    public static void shitPrinter(String stringName){
-        for (int i =0; i< stringName.length(); i++){
-            if(i==0){
-                System.out.print(stringName.charAt(i));
-            }else if(i%2 == 0){
-                System.out.print(stringName.charAt(i));
-            }
-        }
-
-        System.out.print (" ");
-
-        for (int i =0; i< stringName.length(); i++){
-            if(!(i%2==0)){
-                System.out.print(stringName.charAt(i));
-            }
-
-        }
-
-    }
 }
