6c4688fae302442c58ae4b0edc35293a46efcbf4
jenksy <jjenksy@gmail.com>
2017-02-19 12:21:18 -0600

    completed emplyee and salecenter implementations

--- src/cs2/LectureAssignments/programmingassignment1/Employee.java
+++ src/cs2/LectureAssignments/programmingassignment1/Employee.java
@@ -2,9 +2,44 @@ package cs2.LectureAssignments.programmingassignment1;
 
 /**
  * Created by jenksy on 2/19/17.
+ * The Employee class should define an employee’s first and last name, and include an abstract class for calculating pay. A manager’s pay is based on a pay period specified in weeks. Associates pay is by the hour.
+ * The abstract pay() method in Employee will be overridden in Manager and Associate.
  */
-public class Employee {
-    public double pay(double payArg) {
-        return 0;
+public abstract class Employee {
+    private String firstName; //the firstName of the employee
+    private String lastName;// the lastName of the employee
+
+    //no arg constructor
+    public Employee() {
+    }
+
+    /**
+     * Contructor that takes two args to setup employee
+     * @param firstName the first name
+     * @param lastName the last name
+     */
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * The method deifinition for a pay period
+     * @param period the period that requested pay is for
+     * @return the pay for the period
+     */
+    public abstract double pay(double period);
+
+
+    /**
+     * toString method overidden from the Main object
+     * @return the first and last name of the employee
+     */
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
