4d0fa269e3eadc9b14e709addfa91d0ecac21246
John Jenkins <jjenksy@gmail.com>
2017-02-27 12:53:30 -0600

    added btn with callback lambda method

--- src/cs2/javafx.java
+++ src/cs2/javafx.java
@@ -3,6 +3,7 @@ package cs2;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,9 +14,15 @@ public class javafx extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Button btOK = new Button("OK");
+        Button btOK = new Button();
+        btOK.setText("Say, Hello World");
+        btOK.setOnAction(event -> {
+            System.out.println("hello from button.");
+        });
 
-        Scene scene = new Scene(btOK,200, 250);
+        StackPane root = new StackPane();
+        root.getChildren().add(btOK);
+        Scene scene = new Scene(root,200, 250);
 
         primaryStage.setTitle("MyJavaFX");
         primaryStage.setScene(scene);
