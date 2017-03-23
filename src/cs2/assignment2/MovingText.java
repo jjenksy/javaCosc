3dceb09fbebf75afa42d4fbb9230fcee6320da5b
John Jenkins <jjenksy@gmail.com>
2017-03-23 13:28:52 -0500

    worked on Moving Text but failed and suck at life

--- src/cs2/assignment2/MovingText.java
+++ src/cs2/assignment2/MovingText.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -25,26 +24,26 @@ public class MovingText extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Pane pane = new Pane();
-        double width = 400;
-        double height = 400;
-        Scene scene = new Scene(pane, width, height);
-        double radius = Math.min(width, height) * 0.25;
-        Circle c = new Circle(width / 2, height / 2, radius, Color.TRANSPARENT);
+
         Line line = new Line();
         line.setFill(Color.BLACK);//todo fix the line to be follow
-        line.setEndX(50);
-        line.setEndY(100);
-        c.setRotate(180);
-        c.setStroke(Color.BLACK);
+        line.setStroke(Color.BLACK);
+        line.setOpacity(1);
+        line.startXProperty().bind(pane.widthProperty().divide(pane.widthProperty()));
+        line.startYProperty().bind(pane.heightProperty().divide(2));
+        line.endXProperty().bind(pane.widthProperty());
+        line.endYProperty().bind(pane.heightProperty().divide(2));
         Text text = new Text("Programming is fun");
 
+        Scene scene = new Scene(pane, 400, 400);
         PathTransition pathTrans = new PathTransition(Duration.millis(10000), line, text);
         pathTrans.setCycleCount(Timeline.INDEFINITE);
         pathTrans.play();
-
+//        pathTrans.pathProperty().bind(line);
 
         pane.setOnMousePressed(e-> {
             pathTrans.pause();
+
 
         });
         pane.setOnMouseReleased(e-> {
@@ -52,7 +51,7 @@ public class MovingText extends Application {
 
         });
 
-        pane.getChildren().addAll(text);
+        pane.getChildren().addAll(text,line);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Programming is fun");
         primaryStage.show();
