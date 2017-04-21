22b98d46c78eb535d6950e476d4032955f32d0d7
John Jenkins <jjenksy@gmail.com>
2017-04-20 20:25:49 -0500

    added generics directory and class

---
+++ src/cs2/assignment4/GenericStack.java
@@ -0,0 +1,9 @@
+package cs2.assignment4;
+
+/**
+ * Created by jjenkins on 4/20/2017.
+ */
+public class GenericStack<T> {
+    public void push(T java) {
+    }
+}
---
+++ src/cs2/assignment4/Loan.java
@@ -0,0 +1,7 @@
+package cs2.assignment4;
+
+/**
+ * Created by jjenkins on 4/20/2017.
+ */
+public class Loan {
+}
