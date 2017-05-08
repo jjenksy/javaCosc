ca72d4cdb87d6f0e9d03fb95608d0cf1e197e029
jjenksy <jjenksy@gmail.com>
2017-05-08 12:23:21 -0500

    added count and bubble sort

--- src/cs2/assignment4/Bubblesort.java
+++ src/cs2/assignment4/Bubblesort.java
@@ -1,5 +1,11 @@
 package cs2.assignment4;
 
+import cs2.lab5.GeometricObject;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.util.Comparator;
+
 /**
  * Created by johnjenkins on 5/4/17.
  * Write a generic bubble sort method. The bubble sort sorts the elements using the Comparable interface. The method header is as follows.
@@ -17,15 +23,92 @@ package cs2.assignment4;
  */
 public class Bubblesort {
 
-    public static <E extends Comparable<E>> void bubblesort(E[] list){
+    /**
+     * Generic bubble sort method using comparable
+     */
+    public static <E extends Comparable<E>> void bubbleSort(E[] list) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            // Array may be sorted and next pass not needed
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i].compareTo(list[i + 1]) > 0) {
+                    // Swap list[i] with list[i + 1]
+                    E temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; // Next pass still needed
+                }
+            }
+        }
+    }
+
+    /**
+     * Generic bubble sort method using comparator
+     */
+    public static <E> void bubbleSort(E[] list, Comparator<? super E> comparator) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            // Array may be sorted and next pass not needed
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (comparator.compare(list[i], list[i + 1]) > 0) {
+                    // Swap list[i] with list[i + 1]
+                    E temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; // Next pass still needed
+                }
+            }
+        }
 
     }
 
-    public static void main (String[] args){
-        Double[] list = {2.1, 3.5, 2.0, 5.5, 6.1, 1.3, -2.0, 3.1, 14.5, 12.8};
-        bubblesort(list);
+    /** A test method */
+    public static void main(String[] args) {
+        // Create an Integer array
+        Integer[] listArray = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+        // Carate a Double array
+        Double[] doubleArray = {3.4, 1.3, -22.1, 14.8, 6.0, 2.3, 12.2};
+
+        // Create a Character array
+        Character[] charArray = {'a', 'J', 'r'};
+
+        // Create a String array
+        String[] stringArray = {"Tom", "Susan", "Kim"};
+
+        // Sort the arrays
+        bubbleSort(listArray);
+        bubbleSort(doubleArray);
+        bubbleSort(charArray);
+        bubbleSort(stringArray);
+
+        printList(listArray);
+        printList(charArray);
+        printList(stringArray);
+        printList(doubleArray);
+
+        
+    }
+
+    /** Print an array of objects */
+    public static void printList(Object[] list) {
         for (int i = 0; i < list.length; i++)
             System.out.print(list[i] + " ");
+        System.out.println();
+    }
 
+    /** Print the sorted elements */
+    public static void printList(GeometricObject[] list) {
+        System.out.print("Sorted elements: ");
+        for (GeometricObject e: list) {
+            System.out.printf("%.2f ", e.getArea());
+        }
+        System.out.println();
     }
 }
--- src/cs2/assignment4/Count.java
+++ src/cs2/assignment4/Count.java
@@ -1,5 +1,7 @@
 package cs2.assignment4;
 
+import java.util.Scanner;
+
 /**
  * Created by johnjenkins on 5/4/17.
  * Write a recursive method (count) that finds the number of occurrences of a specified character in an array. You need to define the following two methods. The second one is a recursive helper method.
@@ -9,4 +11,30 @@ package cs2.assignment4;
  Write a test program (CharSearch) that prompts the user to enter a list of characters in one line, and then a character to count. Next, it displays the number of occurrences of the character in the list.
  */
 public class Count {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a list of characters in one line: ");
+        String s = input.nextLine();
+        System.out.print("Enter a character: ");
+        char ch = input.next().charAt(0);
+
+        char[] chars = s.replaceAll(" ", "").toCharArray();
+        System.out.println("The occurrences of '" + ch + "' in \"" + s + "\" = " + count(chars, ch));
+    }
+
+    private static int count(char[] chars, char ch) {
+        return count(chars, ch, chars.length - 1);
+    }
+
+    private static int count(char[] chars, char ch, int high) {
+
+        int count = (chars[high] == ch) ? 1 : 0;
+
+        if (high == 0)
+            return count;
+        else
+            return count + count(chars, ch, high - 1);
+    }
 }
