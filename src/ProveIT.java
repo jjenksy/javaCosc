16352e67e6e38738b9760f240b8405a0a586822a
jjenksy <jjenksy@gmail.com>
2017-07-07 09:14:55 -0500

    added proveIT class

---
+++ src/ProveIT.java
@@ -0,0 +1,25 @@
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by johnjenkins on 7/7/17.
+ */
+public class ProveIT {
+
+
+    public ProveIT() {
+    }
+    
+    //Text
+    /* Text */
+    /** Text */
+    /** Text */
+
+
+    public static void main(String[] args){
+      System.out.println(11.0/4.4);
+    }
+}
