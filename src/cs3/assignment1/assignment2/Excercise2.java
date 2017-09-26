2192334883c9e016358fdf43bc39263d4d0a630e
jjenksy <jjenksy@gmail.com>
2017-09-26 16:10:37 -0500

    added Priority Excercise

--- src/cs3/assignment1/assignment2/Excercise2.java
+++ src/cs3/assignment1/assignment2/Excercise2.java
@@ -1,5 +1,8 @@
 package cs3.assignment1.assignment2;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * Created by jenksy on 9/24/17.
  * Write a Java program that creates two priority queues, {"George", "Jim", "John", "Blake",
@@ -7,6 +10,34 @@ package cs3.assignment1.assignment2;
  difference, and intersection.
  */
 public class Excercise2 {
+
+    public static void main(String[] args){
+        PriorityQueue<String> priorityQueue1 = new PriorityQueue<>(Arrays.asList("George", "Jim", "John", "Blake", "Kevin", "Michael"));
+        PriorityQueue<String> priorityQueue2 = new PriorityQueue<>(Arrays.asList("George", "Katie", "Kevin", "Michelle", "Ryan"));
+
+        PriorityQueue<String> union = new PriorityQueue<>(priorityQueue1);
+        PriorityQueue<String> tmp = new PriorityQueue<>(priorityQueue2);
+
+        String s;
+        while ((s = tmp.poll()) != null) {
+            if (!union.contains(s)) {
+                union.add(s);
+            }
+        }
+        System.out.println("Union set: " + union);
+
+        PriorityQueue<String> difference = new PriorityQueue<>(priorityQueue1);
+        difference.removeAll(priorityQueue2);
+        System.out.println("Difference: " + difference);
+
+        PriorityQueue<String> intersection = new PriorityQueue<>(priorityQueue1);
+        intersection.retainAll(priorityQueue2);
+        System.out.println("Difference: " + intersection);
+
+    }
+
+
+
 
 
 }
