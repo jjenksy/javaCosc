ce23a237d474ac1671886d2dfd4f9b2a5f9413f5
jjenksy <jjenksy@gmail.com>
2017-09-26 12:33:56 -0500

    added Streams and filter check to get correct data

--- src/cs3/assignment1/assignment2/Excercise1.java
+++ src/cs3/assignment1/assignment2/Excercise1.java
@@ -1,5 +1,14 @@
 package cs3.assignment1.assignment2;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by jenksy on 9/24/17.
  * Write a Java program that reads words from a text file and displays all the words (duplicates allowed)
@@ -10,15 +19,31 @@ public class Excercise1 {
 
     public static void main (String [] args){
         if(args.length<1){
-            //todo check status codes
             System.exit(1);
         }
-        readWords();
+        readWords(args[0]);
     }
 
 
     
-    private static void readWords() {
+    private static void readWords(String fileName) {
 
+        //create a Stream to retrieve the file contents
+        Stream<String> fileContents = null;
+            try {
+                //get the contents of the files
+                fileContents = Files.lines(Paths.get(fileName));
+            } catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+
+        List<String> noNumbers = fileContents.filter(line-> getValidWords(line)).collect(Collectors.toList());
+
+        System.out.println(noNumbers);
+    }
+
+    public static Boolean getValidWords(String val) {
+        //todo implement check
+        return false;
     }
 }
