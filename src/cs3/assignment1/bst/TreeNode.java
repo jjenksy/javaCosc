304dda253450ae8d2ea2332d5a7f8652cf2ec49c
jjenksy <jjenksy@gmail.com>
2017-11-07 16:40:16 -0600

    added assignment8

---
+++ src/cs3/assignment1/bst/TreeNode.java
@@ -0,0 +1,12 @@
+package cs3.assignment1.bst;
+
+public class TreeNode<E> {
+
+    protected E element;
+    protected TreeNode<E> left;
+    protected TreeNode<E> right;
+
+    public TreeNode(E e) {
+        element = e;
+    }
+}
