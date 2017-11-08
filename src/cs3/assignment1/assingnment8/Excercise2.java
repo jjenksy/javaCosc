630af61ac0e0a96724c4216d2ea27865eb72e205
jjenksy <jjenksy@gmail.com>
2017-11-08 12:55:18 -0600

    Complete BST assignment

--- src/cs3/assignment1/assingnment8/Excercise2.java
+++
@@ -1,9 +0,0 @@
-package cs3.assignment1.assingnment8;
-
-/**
- *Let us consider the following eleven letters: A, E, F, G, H, I, L, M, N, O, S, and T, with the
- following frequencies: 0.04, 0.17, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.10, 0.10, 0.25, and 0.10,
- respectively. Generate the Huffman tree and its associated code.
- */
-public class Excercise2 {
-}
