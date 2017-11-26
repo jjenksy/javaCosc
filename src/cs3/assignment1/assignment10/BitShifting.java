fff83c9a79d3877230d2a8258774778c4b257ed2
jenksy <jjenksy@gmail.com>
2017-11-26 10:40:39 -0600

    completed hashing assignment

---
+++ src/cs3/assignment1/assignment10/BitShifting.java
@@ -0,0 +1,9 @@
+package cs3.assignment1.assignment10;
+
+public class BitShifting {
+
+    public static void main(String[] args){
+        System.out.println((1010110 ^ 0110111));
+        System.out.println((1010110 >> 2));
+    }
+}
